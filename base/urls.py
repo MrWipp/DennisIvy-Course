2f30b4163b6cd62de72633bd8ad05b48ef9ecbd8
MrWipp <Mrwipbussiness@gmail.com>
2023-01-07 15:43:33 +0300

    Added Restriction To Login Required Functions

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name="login-register"),
+    path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutPage, name="logout"),
 
     path('', views.home, name="home"),
