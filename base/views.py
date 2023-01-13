851c690fe597d12ee197779d166573d62c813629
MrWipp <Mrwipbussiness@gmail.com>
2023-01-13 21:29:03 +0300

    Configuring the theme to the backend 2.2

--- base/views.py
+++ base/views.py
@@ -107,13 +107,14 @@ def createRoom(request):
 
     if request.method == 'POST':
         form = RoomForm(request.POST)
+        topics = Topic.objects.all()
         if form.is_valid():
             room = form.save(commit=False)
             room.host = request.user
             room.save()
             return redirect('home')
             
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -121,7 +122,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!!!!')
 
@@ -130,7 +131,7 @@ def updateRoom(request, pk):
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
